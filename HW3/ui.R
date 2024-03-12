ef931bd8aa03a3a6b485b9aa322491995ce8eee6
Sean Zhao <seanzhao@uw.edu>
2024-03-12 03:12:28 -0700

    UI Update for fluid interface

--- HW3/ui.R
+++ HW3/ui.R
@@ -5,24 +5,27 @@
 # Find out more about building applications with Shiny here:
 #
 #    https://shiny.posit.co/
-#
 
+
+
+# Prepare the app ---------------------------------------------------------
+source("Data_Wrangling.R")
 library(shiny)
 
-# Define UI for application that draws a histogram
-fluidPage(# Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(sidebarPanel(
-    sliderInput(
-      "bins",
-      "Number of bins:",
-      min = 1,
-      max = 50,
-      value = 30
+
+# Define the UI -----------------------------------------------------------
+ui <- fluidPage(
+  titlePanel("Gapminder Data"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("x", "X-axis:", choices = c("GDP per cap", "Paved Roads")),
+      selectInput("y", "Y-axis:", choices = c("GDP per cap", "Paved Roads")),
+      selectInput("color", "Color:", choices = c("GDP per cap", "Paved Roads")),
+      selectInput("size", "Size:", choices = c("GDP per cap", "Paved Roads"))
+    ),
+    mainPanel(
+      plotOutput("scatterplot")
     )
-  ),
-  
-  # Show a plot of the generated distribution
-  mainPanel(plotOutput("distPlot"))))
+  )
+)
+
